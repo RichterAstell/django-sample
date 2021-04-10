b85ecf81b45013bab0ad6ef17df5e18cf67a9d49
RichterAstell <xparis1721x@gmail.com>
2021-04-10 20:18:02 +0000

    divide views

--- employee/urls.py
+++ employee/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 app_name = 'employee'
 urlpatterns = [
-    path('search/', views.search, name='search'),
+    path('signin/', views.signin.signin, name='signin'),
+    # path('search/', views.search, name='search'),
+    # path('signin/', views.signin, name='signin'),
+    # path('signin2/', views.signin2, name='signin2'),
+    # path('signout/', views.signout, name='signout'),
     # path('login/', views.Login.as_view(), name='login'),
-    path('signin/', views.signin, name='signin'),
-    path('signin2/', views.signin2, name='signin2'),
-    path('signout/', views.signout, name='signout'),
     # path('', views.index, name='search'),
 ]
---
+++ employee/views/signin.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+
+def signin(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'employee/search.html')
+    else:
+        error_message = "login failed"
+        return render(request, 'employee/login.html', {'error_message': error_message})
+
+def signin2(request):
+    return render(request, 'employee/login.html')
