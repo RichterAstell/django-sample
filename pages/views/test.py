071236d15e65fad63478774e2e5eac3167bc6fad
RichterAstell <xparis1721x@gmail.com>
2021-04-20 18:11:03 +0000

    add err message

--- pages/views/test.py
+++ pages/views/test.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def test(request):
     print(request.POST.get('user', None))
-    print(request.POST.get('pass', None))
+    pa = request.POST.get('pass', None)
+    if (pa == None):
+        error_message = "pass is require"
     return render(
         request,
         'pages/test.html',
+        {"error_message": error_message}
     )
