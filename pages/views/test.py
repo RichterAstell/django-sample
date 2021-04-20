716d6b86c2aa448c8194f390f4a534c04f7b30b1
RichterAstell <xparis1721x@gmail.com>
2021-04-20 22:52:03 +0000

    add form

--- pages/views/test.py
+++ pages/views/test.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def test(request):
+    print(request.POST.get('user', None))
+    print(request.POST.get('pass', None))
     return render(
         request,
         'pages/test.html',
