06b088d4c8b51658b97e08ed20057a535bdddd62
RichterAstell <xparis1721x@gmail.com>
2021-04-21 14:36:17 +0000

    add validation

--- pages/views/test.py
+++ pages/views/test.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def test(request):
-    print(request.POST.get('user', None))
-    pa = request.POST.get('pass', None)
-    if (pa == None):
-        error_message = "pass is require"
+    error_message = []
+    if request.method == 'POST':
+        error_message = __validation(request.POST)
     return render(
         request,
         'pages/test.html',
         {"error_message": error_message}
     )
+
+def __validation(params):
+    error_message = []
+    # user
+    user = params.get('user', None)
+    if (len(user) <5):
+        error_message.append("the length of the user must be at least 5")
+    if (len(user) > 8):
+        error_message.append("the length of the user must be less than or equal to 8")
+    # password
+    password = params.get('password', None)
+    if (password == ''):
+        error_message.append("password is require")
+    return error_message
