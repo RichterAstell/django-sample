40a22473546816eb8ae9c08bacc740f05c64a5c3
RichterAstell <xparis1721x@gmail.com>
2021-04-23 19:23:08 +0000

    retension of input value

--- pages/views/form.py
+++ pages/views/form.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from ..forms import UserForm
 
 def form(request):
+    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
     return render(
         request,
         'pages/form.html',
-        {"form": UserForm()}
+        {"form": form}
     )
